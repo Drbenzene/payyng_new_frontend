d05644e5fe493a868f057d603b33c7c29772c701
Boyinbode Ebenezer Ayomide <78053209+Drbenzene@users.noreply.github.com>
2025-03-01 10:10:28 -0500

    updates

--- app/manifest.ts
+++ app/manifest.ts
@@ -9,19 +9,6 @@ export default function manifest(): MetadataRoute.Manifest {
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#000000",
-    // icons: [
-    //   {
-    //     src: "/icon512_maskable.png",
-    //     sizes: "192x192",
-    //     type: "image/png",
-    //   },
-    //   {
-    //     src: "/icon512_rounded.png",
-    //     sizes: "512x512",
-    //     type: "image/png",
-    //   },
-    // ],
-
     icons: [
       {
         src: "windows11/SmallTile.scale-100.png",
