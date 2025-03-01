cc44ba48b6e320d0be12d72777be14c9cb490811
Boyinbode Ebenezer Ayomide <78053209+Drbenzene@users.noreply.github.com>
2025-03-01 10:03:03 -0500

    updates

--- app/(dashboard)/verification/page.tsx
+++ app/(dashboard)/verification/page.tsx
@@ -1,24 +1,24 @@
 "use client";
 
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import ContentLayout from "@/app/components/layouts/ContentLayout";
 import { validateIdentity } from "@/hooks/useUser";
 import { toast } from "sonner";
 import TransactionSuccess from "@/app/components/modals/TransactionSuccess";
 import dynamic from "next/dynamic";
+import { Camera } from "react-camera-pro";
 
 // Dynamically import the Image component to ensure it's only loaded on the client side
 const Image = dynamic(() => import("next/image"), { ssr: false });
 
 function IDVerification() {
-  const videoRef: any = useRef(null);
-  const canvasRef: any = useRef(null);
   const [step, setStep] = useState(0); // Start with step 0 for ID card type selection
   const [idImage, setIdImage] = useState<any>(null);
   const [faceImage, setFaceImage] = useState<any>(null);
   const [idCardType, setIdCardType] = useState<string>(""); // State for ID card type
   const [loading, setLoading] = useState(false);
   const [showSuccessModal, setShowSuccessModal] = useState(false);
+  const camera = useRef<any>(null);
 
   const submitVerifificationHandler = async () => {
     console.log("Submit Verification");
@@ -31,44 +31,19 @@ function IDVerification() {
     const res = await validateIdentity(payload);
     setLoading(false);
     if (res) {
-      toast.success("Verification submitted successful");
+      toast.success("Verification submitted successfully");
       setShowSuccessModal(true);
     }
   };
 
-  // Start camera only in the browser (client-side)
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const startCamera = async () => {
-        try {
-          const stream = await navigator.mediaDevices.getUserMedia({
-            video: { facingMode: "user" },
-            audio: false,
-          });
-          if (videoRef.current) {
-            videoRef.current.srcObject = stream;
-          }
-        } catch (error) {
-          console.error("Error accessing webcam:", error);
-        }
-      };
-      startCamera();
-    }
-  }, []);
-
   const captureImage = () => {
-    if (canvasRef.current && videoRef.current) {
-      const context = canvasRef.current.getContext("2d");
-      canvasRef.current.width = videoRef.current.videoWidth;
-      canvasRef.current.height = videoRef.current.videoHeight;
-      context.drawImage(videoRef.current, 0, 0);
-      const imageData = canvasRef.current.toDataURL("image/png");
-
+    if (camera.current) {
+      const photo = camera.current.takePhoto();
       if (step === 1) {
-        setIdImage(imageData);
+        setIdImage(photo);
         setStep(2);
       } else {
-        setFaceImage(imageData);
+        setFaceImage(photo);
         setStep(3);
       }
     }
@@ -83,14 +58,15 @@ function IDVerification() {
 
   return (
     <ContentLayout>
-      <div className="min-h-screen flex items-center justify-center bg-gray-900 p-6">
-        <div className="max-w-3xl w-full bg-black p-8 rounded-3xl shadow-2xl text-center text-white border border-gray-700">
-          <h2 className="text-4xl font-bold mb-6 text-white">
+      <div className="min-h-screen flex items-center justify-center bg-gray-900 p-1 md:p-6">
+        <div className="w-full bg-black p-2 md:p-8 rounded-3xl shadow-2xl text-center text-white border border-gray-700">
+          <h2 className="text-xl md:text-3xl font-bold mb-6 text-white">
             {step === 0 && "Select ID Card Type"}
             {step === 1 && "Capture Your ID"}
             {step === 2 && "Capture Your Face"}
             {step === 3 && "Verification Complete"}
           </h2>
+
           {step === 0 && (
             <div className="mb-6">
               <label
@@ -114,18 +90,34 @@ function IDVerification() {
               </select>
             </div>
           )}
+          {(step === 1 || step === 2) && (
+            <div className="">
+              <div className=" min-h-[500px] relative">
+                <Camera
+                  ref={camera}
+                  facingMode={step === 1 ? "environment" : "user"}
+                  aspectRatio="cover"
+                  errorMessages={{
+                    noCameraAccessible:
+                      "No camera device accessible. Please connect your camera or try a different browser.",
+                    permissionDenied:
+                      "Permission denied. Please refresh and give camera permission.",
+                    switchCamera:
+                      "It is not possible to switch camera to different one because there is only one video device accessible.",
+                    canvas: "Canvas is not supported.",
+                  }}
+                />
+              </div>
 
-          {step > 0 && step < 3 && (
-            <div className="relative w-full flex justify-center mb-6">
-              <video
-                ref={videoRef}
-                autoPlay
-                playsInline
-                className="w-96 h-96 rounded-xl border-4 border-blue-500 shadow-lg"
-              />
-              <canvas ref={canvasRef} className="hidden"></canvas>
+              <button
+                onClick={captureImage}
+                className="w-full bg-green-500 hover:bg-green-600 py-3 text-lg font-semibold rounded-xl shadow-lg mt-6"
+              >
+                {step === 1 ? "Capture ID" : "Capture Face"}
+              </button>
             </div>
           )}
+
           {step === 3 && (
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <div>
@@ -154,33 +146,16 @@ function IDVerification() {
               </div>
             </div>
           )}
-          <div className="mt-8 space-y-4">
-            {step === 1 && (
-              <button
-                onClick={captureImage}
-                className="w-full bg-green-500 hover:bg-green-600 py-3 text-lg font-semibold rounded-xl shadow-lg"
-              >
-                Capture ID
-              </button>
-            )}
-            {step === 2 && (
-              <button
-                onClick={captureImage}
-                className="w-full bg-green-500 hover:bg-green-600 py-3 text-lg font-semibold rounded-xl shadow-lg"
-              >
-                Capture Face
-              </button>
-            )}
-            {step === 3 && (
-              <button
-                disabled={loading}
-                onClick={submitVerifificationHandler}
-                className="w-full bg-purple-600 hover:bg-purple-700 py-3 text-lg font-semibold rounded-xl shadow-lg"
-              >
-                {loading ? "PLEASE WAIT..." : "SUBMIT"}
-              </button>
-            )}
-          </div>
+
+          {step === 3 && (
+            <button
+              disabled={loading}
+              onClick={submitVerifificationHandler}
+              className="w-full bg-purple-600 hover:bg-purple-700 py-3 text-lg font-semibold rounded-xl shadow-lg mt-8"
+            >
+              {loading ? "PLEASE WAIT..." : "SUBMIT"}
+            </button>
+          )}
         </div>
       </div>
 
